4ee13f9d0f9fcf6d14376d4d315ed47b5a4db7f3
Ania Nowak <anna.nowak1@relativity.com>
2025-03-13 08:54:25 +0100

    new files

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,3 +1,13 @@
+import {getItemsTotal} from "./etc/get-items-total"
+
 describe("The application",()=>{
   it.todo("should display the correct output") //so it is passed with adnotation it is "TO DO"
+
+  describe("Get items total", ()=>{
+    const specitems = [
+      {name: "item 1", cost: 100},
+      {name: "item 2", cost: 200},
+      {name: "item 3", cost: 300}
+    ]
+  })
 })
